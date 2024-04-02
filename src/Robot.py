ff958079c061ceea9b1c23fb55c5c0ade375c0cb
Derek Baier <derek.m.baier@gmail.com>
2024-04-02 17:48:24 -0400

    Added some testing code in Robot.py

--- src/Robot.py
+++ src/Robot.py
@@ -1,12 +1,29 @@
-from VEXLib.Robot import RobotBase
+from VEXLib.Robot.TelemteryRobot import TelemetryTickBasedRobot
+from vex import Motor, GearSetting, Ports
 
 
-class Robot(RobotBase):
+# print = Telemetry.Telemetry.send_telemetry_message
+
+
+class Robot(TelemetryTickBasedRobot):
     def __init__(self, brain):
         super().__init__(brain)
+        self.motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+        self.i = int(0)
 
     def setup(self):
         print("Setup")
+        self.register_telemetry()
 
     def on_enable(self):
         print("Enable")
+
+    def periodic(self):
+        # if not self.i % 10:
+        print(("autonomous" if self.is_autonomous_control() else "driver control") + " " + ("enabled" if self.is_enabled() else "disabled"))
+        print("runtime:" + str(self.get_enabled_runtime()))
+        self.tick_telemetry()
+        self.telemetry.send_telemetry_message("", "ROBOT_HEARTBEAT")
+        # self.i += 1
+        if self.get_enabled_runtime() > 3.0:
+            self.trigger_restart()
