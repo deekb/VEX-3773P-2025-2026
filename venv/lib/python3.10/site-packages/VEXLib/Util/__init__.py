36428f57e19ee7ada36b7ca7826db5f016769c5a
Derek Baier <derek.m.baier@gmail.com>
2024-04-02 17:49:07 -0400

    Small changes

---
+++ venv/lib/python3.10/site-packages/VEXLib/Util/__init__.py
@@ -0,0 +1,11 @@
+def pass_function(*args, **kwargs):
+    """
+    Takes any arguments and does nothing
+
+    Args:
+        *args: Takes any arguments
+        **kwargs: Takes any arguments
+
+    Returns: None
+    """
+    pass
