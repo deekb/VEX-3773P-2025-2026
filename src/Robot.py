381dcb7f9cbf509738c75f02e4fd8ae58f71765c
Derek Baier <derek.m.baier@gmail.com>
2024-05-23 19:43:06 -0400

    Fixed some telemetry stuff

--- src/Robot.py
+++ src/Robot.py
@@ -1,12 +1,15 @@
 from VEXLib.Robot.TelemteryRobot import TelemetryRobot
+from VEXLib.Robot.RobotBase import RobotBase
+from VEXLib.Robot.TimedRobot import TimedRobot
 from vex import Motor, GearSetting, Ports
 
 
 # print = Telemetry.Telemetry.send_telemetry_message
 
 
-class Robot(TelemetryRobot):
+class Robot(TelemetryRobot, TimedRobot):
     def __init__(self, brain):
+        self.brain = brain
         super().__init__(brain)
         self.motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
         self.i = 0
@@ -21,9 +24,9 @@ class Robot(TelemetryRobot):
     def periodic(self):
         # if not self.i % 10:
         print(("autonomous" if self.is_autonomous_control() else "driver control") + " " + ("enabled" if self.is_enabled() else "disabled"))
-        print("runtime:" + str(self.get_enabled_runtime()))
+        print("runtime:" + str(self.time_since_enable()))
         self.tick_telemetry()
-        self.telemetry.send_telemetry_message("", "ROBOT_HEARTBEAT")
-        # self.i += 1
-        if self.get_enabled_runtime() > 3.0:
+        self.telemetry.send_telemetry_message("ROBOT_HEARTBEAT")
+        self.i += 1
+        if self.time_since_enable() > 3.0:
             self.trigger_restart()
