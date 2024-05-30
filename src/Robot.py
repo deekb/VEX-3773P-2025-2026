cf341fab43b9c18cc1165162883734f066a7dfd1
Derek Baier <derek.m.baier@gmail.com>
2024-05-30 09:23:57 -0400

    Used telemetry.send_message instead of print for example robot code and moved the ROBOT_HEARTBEAT brodcast code to the telemetry class

--- src/Robot.py
+++ src/Robot.py
@@ -1,10 +1,6 @@
 from VEXLib.Robot.TelemteryRobot import TelemetryRobot
-from VEXLib.Robot.RobotBase import RobotBase
 from VEXLib.Robot.TimedRobot import TimedRobot
 from vex import Motor, GearSetting, Ports
-
-
-# print = Telemetry.Telemetry.send_telemetry_message
 
 
 class Robot(TelemetryRobot, TimedRobot):
@@ -12,7 +8,6 @@ class Robot(TelemetryRobot, TimedRobot):
         self.brain = brain
         super().__init__(brain)
         self.motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-        self.i = 0
 
     def setup(self):
         print("Setup")
@@ -22,11 +17,9 @@ class Robot(TelemetryRobot, TimedRobot):
         print("Enable")
 
     def periodic(self):
-        # if not self.i % 10:
-        print(("autonomous" if self.is_autonomous_control() else "driver control") + " " + ("enabled" if self.is_enabled() else "disabled"))
-        print("runtime:" + str(self.time_since_enable()))
         self.tick_telemetry()
-        self.telemetry.send_telemetry_message("ROBOT_HEARTBEAT")
-        self.i += 1
+        self.telemetry.send_message(("autonomous" if self.is_autonomous_control() else "driver control") + " " + ("enabled" if self.is_enabled() else "disabled"))
+        self.telemetry.send_message("runtime:" + str(self.time_since_enable()))
+        self.telemetry.send_message("ROBOT_HEARTBEAT")
         if self.time_since_enable() > 3.0:
             self.trigger_restart()
