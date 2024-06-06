59d1205b786b2c3facff9fb6365f94ffbe80ec57
Derek Baier <derek.m.baier@gmail.com>
2024-06-05 21:03:44 -0400

    Added pyglet for GUI things

---
+++ analyze_PID_test.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+# Read data from file
+data = []
+with open("PID_test.txt", "r") as file:
+    for line in file:
+        if line.startswith("PIDF:"):  # Check if line is not empty
+            measurement = {}
+            entries = line.split(":")
+            entries = entries[1:]  # Discard PIDF header
+            
+            for entry in entries:
+                name, value = entry.split("=")
+                measurement[name] = float(value)
+
+            data.append(measurement)
+
+timestamps = range(len(data))
+
+names = data[0].keys()
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+for name in names:
+    plt.plot(timestamps, [entry[name] for entry in data], label=name)
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.title("Controller Parameters Over Time")
+plt.legend()
+plt.grid(True)
+plt.show()
+
