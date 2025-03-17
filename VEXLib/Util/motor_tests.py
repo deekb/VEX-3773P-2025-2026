670ede3b3a700d3fdf5ef596c4ec8b45c5a9bda1
Derek Baier <derek.m.baier@gmail.com>
2025-03-17 12:21:02 -0400

    Refactor motor data collection to use TimeSeriesLogger, improve data aggregation and documentation

--- VEXLib/Util/motor_tests.py
+++ VEXLib/Util/motor_tests.py
@@ -1,73 +1,82 @@
-import json
-
-from VEXLib.Math import MathUtil
 from VEXLib.Util import time
+from VEXLib.Util.Logging import TimeSeriesLogger
 from vex import TorqueUnits, PERCENT, PowerUnits
 
 
-def collect_power_relationship_data(motor_list, step_delay=0.25, power_step=0.005, max_power=1.0):
+def get_motor_data(motor):
+    """
+    Get data from the motor.
+
+    Args:
+        motor: The motor to get data from.
+
+    Returns:
+        A dictionary containing torque, speed, output power, and efficiency of the motor.
+    """
+    return {
+        "time (s)": time.time(),
+        "torque (Nm)": motor.torque(TorqueUnits.NM),
+        "speed (% of rated)": motor.velocity(PERCENT),
+        "output_power (W)": motor.power(PowerUnits.WATT),
+        "efficiency (%)": motor.efficiency(PERCENT)
+    }
+
+
+def collect_power_relationship_data(filename, motor_list, step_delay=0.05, power_step=0.05, max_power=1.0, data_collection_interval=0, data_points_per_step=1, speed_function=lambda motor: motor.velocity(PERCENT), speed_unit="% of rated"):
     """
     Ramp the input power from 0 to the maximum power and collect data about average torque, power, and efficiency across the motors.
 
     Args:
-        motor_list: A list of motors to run and average
+        filename: The name of the file to log data.
+        motor_list: A list of motors to run and average.
         step_delay: Time (in seconds) to wait between each power step.
-        power_step: Incremental power step (-1 to 1).
-        max_power: Maximum allowable duty cycle -1 to 1.
-
-    Returns:
-        data: A JSON string with input power and average torque, speed, output power, and efficiency data.
+        power_step: Incremental power step (0 to max_power).
+        max_power: Maximum allowable duty cycle (0 to 1).
+        speed_function: The function to get the speed of the motor.
+        data_collection_interval: Time (in seconds) between data collections.
+        data_points_per_step: Number of data points to collect per power step.
     """
-    data = {
-        "input_power": [],
-        "torque": [],
-        "speed": [],
-        "output_power": [],
-        "efficiency": []
-    }
+    logger = TimeSeriesLogger(filename, list(get_motor_data(motor_list[0]).keys()) + ["input_power (% of rated)"])
+
+    for motor in motor_list:
+        motor.set(0)
 
     print("Starting power ramp and data collection...")
 
     for input_power in [i * power_step for i in range(int(max_power / power_step) + 1)]:
+        print("Applying power: {}".format(input_power))
         # Apply power to all motors
         for motor in motor_list:
             motor.set(input_power)
 
-        torques = speeds = output_powers = efficiencies = []
-
         # Wait for the drivetrain to respond
         time.sleep(step_delay)
-        for _ in range(10):
-            torques.extend([motor.torque(TorqueUnits.NM) for motor in motor_list])
-            speeds.extend([motor.velocity(PERCENT) for motor in motor_list])
-            output_powers.extend([motor.power(PowerUnits.WATT) for motor in motor_list])
-            efficiencies.extend([motor.efficiency(PERCENT) for motor in motor_list])
-            time.sleep(0.1)
+        for _ in range(data_points_per_step):
+            aggregated_data = {
+                "time (s)": 0,
+                "torque (Nm)": 0,
+                "speed (% of rated)": 0,
+                "output_power (W)": 0,
+                "efficiency (%)": 0,
+                "input_power (% of rated)": input_power * 100
+            }
+            for motor in motor_list:
+                data = get_motor_data(motor)
+                aggregated_data["time (s)"] += data["time (s)"]
+                aggregated_data["torque (Nm)"] += data["torque (Nm)"]
+                aggregated_data["speed (% of rated)"] += data["speed (% of rated)"]
+                aggregated_data["output_power (W)"] += data["output_power (W)"]
+                aggregated_data["efficiency (%)"] += data["efficiency (%)"]
+                aggregated_data["speed ({})".format(speed_unit)] = speed_function(motor)
 
-        average_torque = MathUtil.average_iterable(torques)
-        average_speed = MathUtil.average_iterable(speeds)
-        average_output_power = MathUtil.average_iterable(output_powers)
-        average_efficiency = MathUtil.average_iterable(efficiencies)
+            # Average the data
+            for key in aggregated_data:
+                if key != "input_power (% of rated)":
+                    aggregated_data[key] /= len(motor_list)
 
-        # Append data to the lists
-        data["input_power"].append(input_power)
-        data["torque"].append(average_torque)
-        data["speed"].append(average_speed)
-        data["output_power"].append(average_output_power)
-        data["efficiency"].append(average_efficiency)
-
-        print(
-            "Input Power: {:.2f}, Speed: {:.2f}%, Torque: {:.2f}Nm, Output Power: {:.2f}W, Efficiency: {:.2f}%".format(
-                input_power,
-                average_speed,
-                average_torque,
-                average_output_power,
-                average_efficiency))
+            logger.write_data(aggregated_data)
+            time.sleep(data_collection_interval)
 
     # Stop the motor group
     for motor in motor_list:
         motor.set(0.0)
-
-    # Serialize data in JSON format
-    json_data = json.dumps(data)
-    return json_data
