60c909d6c0de14d7a63daa4272621db3b30535ff
Derek Baier <derek.m.baier@gmail.com>
2025-03-18 22:41:22 -0400

    Enhance WallStakeMechanism with logging and refactor PID/feedforward parameters

--- src/WallStakeMechanism.py
+++ src/WallStakeMechanism.py
@@ -2,8 +2,9 @@ from Constants import WallStakeMechanismProperties
 from VEXLib.Algorithms.GravitationalFeedforward import GravitationalFeedforward
 from VEXLib.Algorithms.PID import PIDController
 from VEXLib.Geometry.Rotation2d import Rotation2d
-import VEXLib.Math.MathUtil as MathUtil
-from vex import FORWARD, DEGREES, VOLT, Rotation, Thread
+from VEXLib.Util import time
+from VEXLib.Util.Logging import Logger
+from vex import FORWARD, DEGREES, VOLT, Rotation, Thread, Brain
 
 
 class WallStakeState:
@@ -11,6 +12,9 @@ class WallStakeState:
     LOADING = 2
     HIGH_SCORING = 3
     LOW_SCORING = 4
+
+
+wall_stake_mechanism_logger = Logger(Brain().sdcard, Brain().screen, "WallStakeMechanism")
 
 
 class WallStakeMechanism:
@@ -25,15 +29,16 @@ class WallStakeMechanism:
         Initializes the WallStakeMechanism subsystem.
 
         Attributes:
-            motor (vex.Motor): The motor controlling the wall stake mechanism.
-            rotation_sensor (vex.Rotation): The rotation sensor used to measure the mechanism's position.
+            motor (Motor): The motor controlling the wall stake mechanism.
+            rotation_sensor (Rotation): The rotation sensor used to measure the mechanism's position.
         """
-
         self.rotation_sensor = rotation_sensor
         self.motor = motor
 
-        self.PID_TUNINGS = WallStakeMechanismProperties.PID_TUNINGS
-        self.FEEDFORWARD_TUNINGS = WallStakeMechanismProperties.FEEDFORWARD_TUNINGS
+        self.log = wall_stake_mechanism_logger
+
+        self.PID_GAINS = WallStakeMechanismProperties.PID_GAINS
+        self.FEEDFORWARD_GAIN = WallStakeMechanismProperties.FEEDFORWARD_GAIN
         self.DOCKED_POSITION = WallStakeMechanismProperties.DOCKED_POSITION
         self.POSITIONAL_TOLERANCE = WallStakeMechanismProperties.POSITIONAL_TOLERANCE
         self.LOADING_POSITION = WallStakeMechanismProperties.LOADING_POSITION
@@ -41,62 +46,84 @@ class WallStakeMechanism:
         self.LOW_SCORING_POSITION = WallStakeMechanismProperties.LOW_SCORING_POSITION
         self.UPRIGHT_POSITION = WallStakeMechanismProperties.UPRIGHT_POSITION
 
-        self.gravitational_feedforward = GravitationalFeedforward(self.FEEDFORWARD_TUNINGS["kg"])
-        self.pid = PIDController(self.PID_TUNINGS["kp"], self.PID_TUNINGS["ki"], self.PID_TUNINGS["kd"])
+        self.gravitational_feedforward = GravitationalFeedforward(self.FEEDFORWARD_GAIN)
+        self.pid = PIDController(self.PID_GAINS)
 
         self.state = WallStakeState.DOCKED
+
         # Call transition_to to ensure we are really in the initial state
         self.transition_to(self.state)
+        self.log.info("WallStakeMechanism initialized with state: DOCKED")
+        self.log.info("Starting loop thread")
         self.tick_thread = Thread(self.loop)
 
     def update_motor_voltage(self):
+        self.log.trace("Entering update_motor_voltage")
         current_rotation = Rotation2d.from_degrees(self.rotation_sensor.position(DEGREES))
         feedforward_output = self.gravitational_feedforward.update(current_rotation.to_degrees())
         pid_output = self.pid.update(current_rotation.normalize().to_revolutions())
 
+        self.log.debug("Current rotation: {} degrees".format(current_rotation.to_degrees()))
+        self.log.debug("Feedforward output: {}, PID output: {}".format(feedforward_output, pid_output))
+
         if self.state == WallStakeState.DOCKED and self.at_setpoint():
             self.motor.spin(FORWARD, 0, VOLT)
+            self.log.info("Motor stopped as the mechanism is docked and at setpoint")
             return
 
         self.motor.spin(FORWARD, feedforward_output - pid_output, VOLT)
+        self.log.debug("Motor voltage set to: {} VOLT".format(feedforward_output - pid_output))
 
+    @wall_stake_mechanism_logger.logged
     def transition_to(self, new_state):
         if new_state == WallStakeState.DOCKED:
-            print("Transitioning to Docked")
+            self.log.info("Transitioning to Docked")
             self.pid.setpoint = self.DOCKED_POSITION.to_revolutions()
         elif new_state == WallStakeState.LOADING:
-            print("Transitioning to Loading")
+            self.log.info("Transitioning to Loading")
             self.pid.setpoint = self.LOADING_POSITION.to_revolutions()
         elif new_state == WallStakeState.HIGH_SCORING:
-            print("Transitioning to High Scoring")
+            self.log.info("Transitioning to High Scoring")
             self.pid.setpoint = self.HIGH_SCORING_POSITION.to_revolutions()
         elif new_state == WallStakeState.LOW_SCORING:
-            print("Transitioning to Low Scoring")
+            self.log.info("Transitioning to Low Scoring")
             self.pid.setpoint = self.LOW_SCORING_POSITION.to_revolutions()
 
         self.state = new_state
+        self.log.debug("New state set: {}".format(new_state))
 
     def next_state(self):
+        self.log.trace("Entering next_state")
         if self.state == 4:
+            self.log.warn("Already in the highest state, cannot transition to next state")
             return
         self.transition_to(self.state + 1)
+        self.log.info("Transitioned to next state:", self.state)
 
     def previous_state(self):
+        self.log.trace("Entering previous_state")
         if self.state == 1:
+            self.log.warn("Already in the lowest state, cannot transition to previous state")
             return
         self.transition_to(self.state - 1)
+        self.log.info("Transitioned to previous state:", self.state)
 
     def tick(self):
+        self.log.trace("Entering tick")
         if self.state == WallStakeState.DOCKED:
-            self.gravitational_feedforward.kg = self.FEEDFORWARD_TUNINGS["kg"] * -1
+            self.gravitational_feedforward.kg = self.FEEDFORWARD_GAIN * -1
         else:
-            self.gravitational_feedforward.kg = self.FEEDFORWARD_TUNINGS["kg"]
+            self.gravitational_feedforward.kg = self.FEEDFORWARD_GAIN
 
         self.update_motor_voltage()
 
     def loop(self):
+        self.log.trace("Entering loop")
         while True:
             self.tick()
+            time.sleep_ms(10)
 
     def at_setpoint(self):
-        return self.pid.at_setpoint(self.POSITIONAL_TOLERANCE.to_revolutions())
+        at_setpoint = self.pid.at_setpoint(self.POSITIONAL_TOLERANCE.to_revolutions())
+        self.log.debug("At setpoint:", at_setpoint)
+        return at_setpoint
