3e686b0de30a2194234ce5756eed0d327ba046fc
Derek Baier <derek.m.baier@gmail.com>
2025-03-20 15:32:43 -0400

    Refactor PID gains and add CornerMechanism driver in Constants.py

--- src/Constants.py
+++ src/Constants.py
@@ -1,13 +1,25 @@
-from VEXLib.Algorithms.PID import PIDGains
-from VEXLib.Algorithms.PIDF import PIDFGains
 from VEXLib.Geometry.GeometryUtil import circle_circumference
 from VEXLib.Geometry.Rotation2d import Rotation2d
 from VEXLib.Geometry.Translation1d import Distance
 from VEXLib.Geometry.Velocity1d import Velocity1d
+from CornerMechanism import Sides
 from vex import Ports, GearSetting, Brain
 
 MAIN_LOG_FILENAME = "main"
 DEBUG_LOG_FILENAME = "debug"
+
+
+class PIDGains:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+
+class PIDFGains(PIDGains):
+    def __init__(self, kp, ki, kd, kf=0.0):
+        super().__init__(kp, ki, kd)
+        self.kf = kf
 
 
 class ControlStyles:
@@ -24,7 +36,9 @@ class DefaultPreferences:
     COLOR_SORT = False
     MEASURE_DRIVETRAIN_PROPERTIES_ON_STARTUP = False
     USE_PIDF_CONTROL = False
+    PIDF_GAINS = PIDFGains(0, 0, 0, 0.6)
     ENABLE_DRIVING = True
+    CORNER_MECHANISM_DRIVER = Sides.RIGHT
 
 
 class DirkPreferences(DefaultPreferences):
@@ -36,8 +50,9 @@ class DirkPreferences(DefaultPreferences):
 
 class DerekPreferences(DefaultPreferences):
     CONTROL_STYLE = ControlStyles.SPLIT_ARCADE
-    CUBIC_FILTER_LINEARITY = 1
+    CUBIC_FILTER_LINEARITY = 0.7
     USE_PIDF_CONTROL = True
+    PIDF_GAINS = PIDFGains(0.1, 0, 0, 0.6)
     ENABLE_DRIVING = False
 
 
@@ -81,17 +96,17 @@ class GearRatios:
 
 class DrivetrainProperties:
     # PID gains
-    LEFT_PIDF_GAINS = PIDFGains(0.15, 0, 0, 0.576953 * 1.1)
-    RIGHT_PIDF_GAINS = PIDFGains(0.15, 0, 0, 0.5742773 * 1.1)
+    LEFT_PIDF_GAINS = PIDFGains(0.2, 0, 0, 0.6)
+    RIGHT_PIDF_GAINS = PIDFGains(0.2, 0, 0, 0.6)
 
-    POSITION_PID_GAINS = PIDGains(5, 0.1, 0)
-    ROTATION_PID_GAINS = PIDGains(0.9, 0.0, 0.04)
+    POSITION_PID_GAINS = PIDGains(5, 1, 0)
+    ROTATION_PID_GAINS = PIDGains(0.75, 0.0, 0.02)
 
     ROBOT_RELATIVE_TO_FIELD_RELATIVE_ROTATION = Rotation2d.from_degrees(90)
     TURN_TIMEOUT_SECONDS = 2
     TURN_CORRECTION_SCALAR_WHILE_MOVING = 0.9
     TURNING_THRESHOLD = Rotation2d.from_degrees(3)
-    MOVEMENT_DISTANCE_THRESHOLD = Distance.from_centimeters(0.1)
+    MOVEMENT_DISTANCE_THRESHOLD = Distance.from_centimeters(0.5)
     MOVEMENT_MAX_EXTRA_TIME = 1
     MAX_ACHIEVABLE_SPEED = Velocity1d.from_meters_per_second(1.6)
     MOTOR_TO_WHEEL_GEAR_RATIO = (36 / 60)
