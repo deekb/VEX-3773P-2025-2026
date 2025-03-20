b1dc447a6401447b1849185e438e60109beeb77a
Derek Baier <derek.m.baier@gmail.com>
2025-03-20 15:32:54 -0400

    Refactor CornerMechanism to support dual piston control and add Sides enumeration

--- src/CornerMechanism.py
+++ src/CornerMechanism.py
@@ -1,10 +1,20 @@
+class Sides:
+    LEFT = 1
+    RIGHT = 2
+
+
 class CornerMechanism:
-    def __init__(self, digital_out):
-        self.corner_mechanism_piston = digital_out
+    def __init__(self, left_digital_out, right_digital_out):
+        self.left_corner_mechanism_piston = left_digital_out
+        self.right_corner_mechanism_piston = right_digital_out
         self.corner_mechanism_state = False
+        self.active_side = Sides.LEFT
 
     def _update_state(self):
-        self.corner_mechanism_piston.set(self.corner_mechanism_state)
+        if self.active_side == Sides.LEFT:
+            self.left_corner_mechanism_piston.set(self.corner_mechanism_state)
+        elif self.active_side == Sides.RIGHT:
+            self.right_corner_mechanism_piston.set(self.corner_mechanism_state)
 
     def lower_corner_mechanism(self):
         self.corner_mechanism_state = True
