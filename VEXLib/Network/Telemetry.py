882f5f48e6a1b35c20076ab6d0e9bb9caf921f08
Derek Baier <derek.m.baier@gmail.com>
2025-03-20 15:34:00 -0400

    Refactor serial communication in Telemetry.py for improved message handling and processing

--- VEXLib/Network/Telemetry.py
+++ VEXLib/Network/Telemetry.py
@@ -6,220 +6,54 @@ import sys
 brain = Brain()
 
 
-class SerialCommunicationProtocol:
-    def __init__(self):
-        self.incoming_messages = SafeList()
-        self.outgoing_messages = SafeList()
+class SerialCommunication:
+    def __init__(self, tx_port, rx_port):
+        self.tx_port = open(tx_port, "wb")
+        self.rx_port = open(rx_port, "rb")
+        self.transmits = []
+        self.receives = []
+        self.receive_thread = Thread(self.receive_loop)
+        self.transmit_thread = Thread(self.transmit_loop)
 
-        Thread(self.get_loop)
-        Thread(self.send_loop)
+    def process_transmits(self):
+        if self.transmits:
+            self.tx_port.write(self.transmits.pop(0) + "\n")
 
-    def get_loop(self):
-        while True:
-            self.incoming_messages.append(sys.stdin.readline().strip("\n"))
-
-    def send_loop(self):
-        while True:
-            if self.has_outgoing_messages():
-                sys.stdout.write(str(self.outgoing_messages.pop(0)) + "\n")
-
-    def has_incoming_messages(self):
-        return bool(self.incoming_messages)
-
-    def has_outgoing_messages(self):
-        return bool(self.outgoing_messages)
+    def process_receives(self):
+        got = self.rx_port.read(1024)
+        if got:
+            self.receives.append(got.decode())
 
     def send(self, message):
-        self.outgoing_messages.append(message)
+        self.transmits.append(message)
 
-    def receive(self, blocking=False):
-        if blocking:
-            while not self.has_incoming_messages():
+    def receive(self, block=False):
+        if block:
+            while not self.receives:
                 pass
-        if self.has_incoming_messages():
-            return self.incoming_messages.pop(0)
+        if self.receives:
+            received = self.receives.pop(0)
+            if received.endswith("\n"):
+                received = received[:-1]
+            return received
         return None
 
-    def peek(self, blocking=False):
-        if blocking:
-            while not self.has_incoming_messages():
-                pass
-        if self.has_incoming_messages():
-            try:
-                return self.incoming_messages[0]
-            except IndexError:
-                return None
-        return None
+    def peek(self, block=False):
+        if self.receives:
+            return self.receives[-1]
+        else:
+            return None
 
-    def read_file(self):
-        file_contents = ""
-        self.send("Ready to receive file, waiting for filename")
-        while not self.has_incoming_messages():
-            pass
-        filename = self.receive()
-        brain.screen.print("Receiving file: " + filename)
-        brain.screen.next_row()
-        self.send("Receiving file: " + filename)
+    def peek_buffer(self, block=False):
+        if self.receives:
+            return "".join(self.receives)
+        else:
+            return None
 
+    def transmit_loop(self):
         while True:
-            chunk = self.receive(True)
-            brain.screen.print("Read chunk: " + chunk)
-            brain.screen.next_row()
+            self.process_transmits()
 
-            if FILE_TERMINATION_CHARACTER not in chunk:
-                file_contents += chunk + "\n"
-            else:
-                break
-        return filename, file_contents
-
-
-class HalfDuplexUARTCommunicationProtocol:
-    def __init__(self):
-        self.incoming_messages = SafeList()
-        self.outgoing_messages = SafeList()
-
-        self.file_handle = open("/dev/port20", "rw")
-        self.TOKEN_CHARACTER = "TKN"
-        self.has_token = False
-
-        Thread(self.get_loop)
-        Thread(self.send_loop)
-
-    def get_loop(self):
+    def receive_loop(self):
         while True:
-            got = self.file_handle.readline()
-            if self.TOKEN_CHARACTER in got:
-                self.has_token = True
-            if got:
-                self.incoming_messages.append(got)
-
-    def send_loop(self):
-        while True:
-            if self.has_token and self.has_outgoing_messages():
-                self.file_handle.write(str(self.outgoing_messages.pop(0)) + self.TOKEN_CHARACTER + "\n")
-
-    def has_incoming_messages(self):
-        return bool(self.incoming_messages)
-
-    def has_outgoing_messages(self):
-        return bool(self.outgoing_messages)
-
-    def send(self, message):
-        self.outgoing_messages.append(message)
-
-    def receive(self, blocking=False):
-        if blocking:
-            while not self.has_incoming_messages():
-                pass
-        if self.has_incoming_messages():
-            return self.incoming_messages.pop(0)
-        return None
-
-    def peek(self, blocking=False):
-        if blocking:
-            while not self.has_incoming_messages():
-                pass
-        if self.has_incoming_messages():
-            try:
-                return self.incoming_messages[0]
-            except IndexError:
-                return None
-        return None
-
-    # def read_file(self):
-    #     file_contents = ""
-    #     self.send("Ready to receive file, waiting for filename")
-    #     while not self.has_incoming_messages():
-    #         pass
-    #     filename = self.receive()
-    #     brain.screen.print("Receiving file: " + filename)
-    #     brain.screen.next_row()
-    #     self.send("Receiving file: " + filename)
-    #
-    #     while True:
-    #         chunk = self.receive(True)
-    #         brain.screen.print("Read chunk: " + chunk)
-    #         brain.screen.next_row()
-    #
-    #         if FILE_TERMINATION_CHARACTER not in chunk:
-    #             file_contents += chunk + "\n"
-    #         else:
-    #             break
-    #     return filename, file_contents
-
-
-class Telemetry:
-    def __init__(self):
-        # self.serial = SerialCommunicationProtocol()
-        self.serial = HalfDuplexUARTCommunicationProtocol()
-
-        self.telemetry_entries: list[TelemetryEntry] = []
-
-    def register_entry(self, entry):
-        self.telemetry_entries.append(entry)
-
-    def update(self):
-        self.send_message("ROBOT_HEARTBEAT")
-        # for entry in self.telemetry_entries:
-        #     self.serial.send("TELEMETRY:" + str(entry.name) + "|" + str(entry.get_value()))
-
-    def get_message(self):
-        return self.serial.receive(blocking=False)
-
-    def send_message(self, message):
-        self.serial.send(message)
-
-
-class TelemetryEntry:
-    def __init__(self, name):
-        self.name = name
-        self.value = None
-
-    def set_value(self, value):
-        self.value = value
-
-    def get_value(self):
-        return self.value
-
-
-class Integer(TelemetryEntry):
-    def __init__(self, integer, name):
-        super().__init__(name)
-        self.value = integer
-
-    def set_value(self, value):
-        self.value = value
-
-    def get_value(self):
-        return self.value
-
-    def __repr__(self):
-        return "TelemetryInteger(" + str(self.value) + ")"
-
-
-class MotorEntry(TelemetryEntry):
-    def __init__(self, motor, name):
-        super().__init__(name)
-        self.motor = motor
-
-    def set_value(self, value):
-        raise NotImplementedError("Can't set the value of a Motor entry")
-
-    def get_value(self):
-        if self.motor.installed():
-            return "Motor running at speed: " + str(self.motor.velocity(PERCENT))
-        return "Motor unplugged"
-
-
-class InertialEntry(TelemetryEntry):
-    def __init__(self, inertial, name):
-        super().__init__(name)
-        self.inertial = inertial
-
-    def set_value(self, value):
-        raise NotImplementedError("Can't set the value of an Inertial entry")
-
-    def get_value(self):
-        if self.inertial.installed():
-            return "Inertial: pitch: " + str(self.inertial.pitch(DEGREES)) + " roll: " + str(self.inertial.roll(DEGREES)) + " yaw: " + str(self.inertial.yaw(DEGREES))
-        return "Inertial unplugged"
+            self.process_receives()
